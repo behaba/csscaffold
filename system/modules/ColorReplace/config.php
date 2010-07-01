cef21709079e190063d5e7048f8cd3a2122fc655
Olivier <og@agencecosmic.com>
2010-07-01 11:11:50 +0200

    Old file deleted

--- system/modules/ColorReplace/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = array(
-	'convert_unvalid_colors' => true, // allows to convert unvalid W3C colors
-	'convert_hsl_colors' => true, // allows to convert HSL Colors
-	'convert_hsla_colors' => true, // allows to convert HSL Colors with alpha specification
-	'convert_cmyk_colors' => true, // allows to convert CMYK Colors
-	'convert_cmyka_colors' => true, // allows to convert CMYK Colors with alpha specification
-	'shorten_hexadecimal_colors' => true // allow to shorten hexadecimal colors
-);
